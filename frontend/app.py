4dd6d83b181b6c14a26e6463b4ef07ebd86e28e8
root <root@ip-172-31-13-248.ap-south-1.compute.internal>
2026-02-02 18:07:15 +0000

    springbot

--- frontend/app.py
+++ frontend/app.py
@@ -349,7 +349,7 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 # API URL
-API_URL = os.environ.get("API_URL", "http://54.218.48.169:8084")
+API_URL = os.environ.get("API_URL", "http://13.235.80.151:8084")
 
 # Tabs with emojis and clear names
 tab1, tab2, tab3, tab4, tab5 = st.tabs([
